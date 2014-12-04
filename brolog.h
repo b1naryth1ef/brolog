f10633303055cb8f9d17141207233210ec68075d
Andrei <b1naryth1ef@gmail.com>
2014-12-03 20:35:58 -0600

    Include local file source for multi-ostream writer

--- brolog.h
+++ brolog.h
@@ -13,12 +13,31 @@ This is a simple logger class that can be used to debug or log issues
 #include <stdarg.h>
 #include <time.h>
 
-#include "ioutil.h"
+// Multiwriter is a simple class that allows writing to multiple ostream objects at once.
+class MultiWriter {
+    private:
+        std::vector<std::ostream*> mwli;
+
+    public:
+        MultiWriter() {}
+
+        // Add an ostream pointer to the list of printables
+        void add(std::ostream *f) {
+            mwli.push_back(f);
+        }
+
+        // Write to the ostreams (all at once)
+        void write(const char* s, std::streamsize n) {
+            for ( auto &i : mwli ) {
+                i->write(s, n);
+            }
+        }
+};
 
 class Log {
     public:
         std::ofstream *file;
-        ioutil::MultiWriter mw;
+        MultiWriter mw;
 
         Log() {
             mw.add(&std::cout);
